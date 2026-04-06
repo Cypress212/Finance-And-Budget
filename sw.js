b0699dba73402b5141e9f6b619f7ae6f1b21cc52
Cypress212 <118564151+Cypress212@users.noreply.github.com>
2026-04-06 13:09:52 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -22,32 +22,3 @@ self.addEventListener('fetch', (event) => {
   );
 });
 
-
-function previewMedia(input) {
-    if (input.files && input.files[0]) {
-        const reader = new FileReader();
-        reader.onload = (e) => {
-            const img = new Image();
-            img.src = e.target.result;
-            img.onload = () => {
-                // Create a hidden canvas to resize the image
-                const canvas = document.createElement('canvas');
-                const MAX_WIDTH = 600; 
-                const scaleSize = MAX_WIDTH / img.width;
-                canvas.width = MAX_WIDTH;
-                canvas.height = img.height * scaleSize;
-
-                const ctx = canvas.getContext('2d');
-                ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-
-                // Convert to low-quality JPEG to save massive space
-                receiptData = canvas.toDataURL('image/jpeg', 0.5); 
-                
-                const p = document.getElementById('receipt-preview');
-                p.src = receiptData;
-                p.style.display = 'block';
-            };
-        };
-        reader.readAsDataURL(input.files[0]);
-    }
-}
